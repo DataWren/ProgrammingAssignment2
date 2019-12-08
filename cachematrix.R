baa1ab5a0c7fdf9600550bc0bc73b2ffa2f4ffa1
Fudeman <kirsten.fudeman@thermofisher.com>
2019-12-08 09:28:44 -0500

    edit R programming asst 2 cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming assignment 2
+## Created Date: December 8, 2019
+# 
+# Description:
+# This function is able to cache potentially time-consuming 
+#       computations, namely calculating the inverse of a
+#       matrix. This function takes advantage of the 
+#       scoping rules of the R language and how they can be 
+#       manipulated to preserve state inside of an R object.
+# First, we create a matrix object
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {   
+  m <- NULL        
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# In code above, we initialized x as function argument.
+# We then initialized m and set it to NULL for later use.
+# The code above also provides for "getters and settters." 
+#        Getters retrieve data within an object, and 
+#        setters set the data within an object.
+# Last section of code assigns each of the functions created 
+#       as an element within a list(), and returns it 
+#       to the parent environment.
+# The fully formed object makeCacheMatrix will be used by 
+#       the next section of code.
+# The next section of code returns a matrix that is 
+#       the inverse of 'x'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinv(m)
+    m
+  }
+
+## TEST  
+# Example: A simple matrix m1 with a simple matrix inverse n1
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+# Running this code ...
+myMatrix_object <- makeCacheMatrix(m1)
+cacheSolve(myMatrix_object)
+# ... should return n1
+n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
+
+
